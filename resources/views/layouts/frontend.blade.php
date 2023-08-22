27f32c34e8ca2684154eeff0525edf17ebee1e4e
Hugeng <setokuncorogo@gmail.com>
2023-08-22 13:38:27 +0700

    Update 2023-08-22

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -760,29 +760,6 @@
     <!-- ======= Footer ======= -->
     <footer id="footer">
 
-        <div class="footer-newsletter">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <!-- Histats.com  (div with counter) -->
-                    <div id="histats_counter"></div>
-                    <a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4789001&101" alt="" border="0"></a>
-                    <!-- Histats.com  START  (aync)-->
-                    <script type="text/javascript">
-                        var _Hasync= _Hasync|| [];
-                        _Hasync.push(['Histats.start', '1,4789001,4,15,170,40,00011111']);
-                        _Hasync.push(['Histats.fasi', '1']);
-                        _Hasync.push(['Histats.track_hits', '']);
-                        (function() {
-                            var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-                            hs.src = ('//s10.histats.com/js15_as.js');
-                            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-                        })();
-                    </script>
-                    <!-- Histats.com  END  -->
-                </div>
-            </div>
-        </div>
-
         <div class="footer-top">
             <div class="container">
                 <div class="row">
